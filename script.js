67e10a41bf3105fb22ae7ee1134e56929dd5b6a0
Franco-Redivo <144297778+Franco-Redivo@users.noreply.github.com>
2024-07-27 20:54:39 -0300

    create function getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("hello");
+//a variable storing the users input choice
+let userChoice;
+
+//getComputerChoice function creates a random number between 0 and 2.
+function getComputerChoice(){
+    let randomNumber = Math.floor(Math.random() * 3);
+    let choice;
+    
+    if(randomNumber === 0){
+        choice = "rock";
+    }else if(randomNumber === 1){
+        choice = "paper";
+    }else{
+        choice = "scissors";
+    }
+
+    return choice;
+}
+
+let computerChoice = getComputerChoice();
+
+
+
+
+
+//compare the 2 variables to see who one
+
+//output telling the user who won
